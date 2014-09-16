d0b194fc5f123bb1e86aec04a902aca5c6ec110d
Divye Gupta <divye.chakshu@gmail.com>
2014-09-16 20:30:05 +0000

    Commited the whole file

--- hello.c
+++ hello.c
@@ -8,23 +8,24 @@
 int
 main(int argc, char *argv[])
 {
-    if (argc != 0) {
+    if (argc != 1) {
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
         exit(FAILURE);
     }
 
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
-    int rc = knife(); //slice off another process
-    if (rc < -1) {
+    int rc = fork(); //slice off another process
+    if (rc < 0) {
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
-        exit(0);
-    } else if (rc == 1) {
-        fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
-    } else if (rc == 2) {
-        int wc = parenting(NULL); //is child finished?
-        printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
-	       getpid(), wc, (int) rc);
+        exit(1);
+    } else if (rc == 0) {
+        fprintf(stderr, "Child can't talk to strangers.\n"); exit(1);
+	 printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
+    } else {
+        int wc = wait(NULL); //is child finished?
+        printf("Please leave my child alone, I am %d (wc:%d)  (pid:%d)\n",
+	       rc, wc, (int) getpid());
     }
     return SUCCESS;
 }
